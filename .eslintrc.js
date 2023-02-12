304d58d8efcd674cfc2c2e94df45944a55c7e127
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-12 21:48:34 +0400

    Add configs

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  ignorePatterns: ['dist', 'node_modules'],
+  root: true,
+  rules: {
+    'arrow-parens': 1,
+    camelcase: 1,
+    'max-params': ['error', 2],
+    'no-duplicate-imports': 1,
+    'no-restricted-syntax': [
+      1,
+      {
+        selector: 'ExportDefaultDeclaration',
+        message: 'Prefer named exports.',
+      },
+    ],
+    'no-tabs': 1,
+  },
+}
