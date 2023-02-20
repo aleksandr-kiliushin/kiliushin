953f55b7152f34c450cdd39d65645c5d45c1d9fa
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-20 08:44:48 +0400

    chore: Allow 3 arguments to functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,21 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended'],
-  ignorePatterns: ['dist', 'node_modules'],
+  extends: ["eslint:recommended"],
+  ignorePatterns: ["dist", "node_modules"],
   root: true,
   rules: {
-    'arrow-parens': 1,
+    "arrow-parens": 1,
     camelcase: 1,
-    'max-params': ['error', 2],
-    'no-duplicate-imports': 1,
-    'no-restricted-syntax': [
+    "max-params": ["error", 3],
+    "no-duplicate-imports": 1,
+    "no-restricted-syntax": [
       1,
       {
-        selector: 'ExportDefaultDeclaration',
-        message: 'Prefer named exports.',
+        selector: "ExportDefaultDeclaration",
+        message: "Prefer named exports.",
       },
     ],
-    'no-tabs': 1,
+    "no-tabs": 1,
   },
 }
