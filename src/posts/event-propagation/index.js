8e8a951375e7b4c3fa538bda9f33f5973b8241e4
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-21 11:16:31 +0400

    Add event listeners to all demo layers

--- src/posts/event-propagation/index.js
+++ src/posts/event-propagation/index.js
@@ -1,5 +1,15 @@
-const wait = () => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 300)
+const elementsIds = ["html", "body", "form", "input", "textarea", "button"]
+
+elementsIds.forEach((elementId) => {
+  const element = document.querySelector(`.demo #${elementId}`)
+  element.addEventListener("click", () => {
+    console.log({ element: elementId, stage: "bubbling" })
   })
-}
+  element.addEventListener(
+    "click",
+    () => {
+      console.log({ element: elementId, stage: "capturing" })
+    },
+    { capture: true }
+  )
+})
