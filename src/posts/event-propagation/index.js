9ec6144832d0a85ba5a3c5383ca9e18b90193337
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-21 12:43:44 +0400

    Add curren event target highlighting

--- src/posts/event-propagation/index.js
+++ src/posts/event-propagation/index.js
@@ -9,17 +9,23 @@ elementsIds.forEach((elementId) => {
     () => {
       eventsAmount += 1
       setTimeout(() => {
-        console.log({ element: elementId, stage: "capturing" })
+        element.classList.add("active")
+      }, eventsAmount * 1000 + 500)
+      setTimeout(() => {
+        element.classList.remove("active")
         eventsAmount -= 1
-      }, eventsAmount * 500 + 500)
+      }, eventsAmount * 1000 + 1000)
     },
     { capture: true }
   )
   element.addEventListener("click", () => {
     eventsAmount += 1
     setTimeout(() => {
-      console.log({ element: elementId, stage: "capturing" })
+      element.classList.add("active")
+    }, eventsAmount * 1000 + 500)
+    setTimeout(() => {
+      element.classList.remove("active")
       eventsAmount -= 1
-    }, eventsAmount * 500 + 500)
+    }, eventsAmount * 1000 + 1000)
   })
 })
