2781edc9e85568a74e406920ef08c8357d9e9e06
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-21 21:05:07 +0400

    feat: Arrows appear for input

--- src/posts/event-propagation/index.js
+++ src/posts/event-propagation/index.js
@@ -2,6 +2,47 @@ const elementsIds = ["html", "body", "form", "input", "textarea", "button"]
 
 let eventsAmount = 0
 
+const toggleArrow = ({ currentTargetId, phase }) => {
+  let arrow = null
+
+  if (phase === "capturing") {
+    if (currentTargetId === "body") {
+      arrow = document.querySelector("#arrow-from-html-to-body")
+    }
+    if (currentTargetId === "form") {
+      arrow = document.querySelector("#arrow-from-body-to-form")
+    }
+    if (currentTargetId === "input") {
+      arrow = document.querySelector("#arrow-from-form-to-input")
+    }
+    if (currentTargetId === "textarea") {
+      arrow = document.querySelector("#arrow-from-form-to-textarea")
+    }
+    if (currentTargetId === "button") {
+      arrow = document.querySelector("#arrow-from-form-to-button")
+    }
+  }
+
+  if (phase === "bubbling") {
+    if (currentTargetId === "html") {
+      arrow = document.querySelector("#arrow-from-body-to-html")
+    }
+    if (currentTargetId === "body") {
+      arrow = document.querySelector("#arrow-from-form-to-body")
+    }
+    if (currentTargetId === "form") {
+      arrow = document.querySelector("#arrow-from-input-to-form")
+    }
+  }
+
+  if (arrow === null) return
+
+  arrow.style.visibility = "visible"
+  setTimeout(() => {
+    arrow.style.visibility = "hidden"
+  }, 1000)
+}
+
 elementsIds.forEach((elementId) => {
   const element = document.querySelector(`.demo #${elementId}`)
   element.addEventListener(
@@ -10,11 +51,14 @@ elementsIds.forEach((elementId) => {
       eventsAmount += 1
       setTimeout(() => {
         element.classList.add("active")
-      }, eventsAmount * 1000 + 500)
+      }, eventsAmount * 1500 + 500)
+      setTimeout(() => {
+        toggleArrow({ currentTargetId: elementId, phase: "capturing" })
+      }, eventsAmount * 1500)
       setTimeout(() => {
         element.classList.remove("active")
         eventsAmount -= 1
-      }, eventsAmount * 1000 + 1000)
+      }, eventsAmount * 1500 + 1500)
     },
     { capture: true }
   )
@@ -22,10 +66,13 @@ elementsIds.forEach((elementId) => {
     eventsAmount += 1
     setTimeout(() => {
       element.classList.add("active")
-    }, eventsAmount * 1000 + 500)
+    }, eventsAmount * 1500 + 500)
+    setTimeout(() => {
+      toggleArrow({ currentTargetId: elementId, phase: "bubbling" })
+    }, eventsAmount * 1500)
     setTimeout(() => {
       element.classList.remove("active")
       eventsAmount -= 1
-    }, eventsAmount * 1000 + 1000)
+    }, eventsAmount * 1500 + 1500)
   })
 })
