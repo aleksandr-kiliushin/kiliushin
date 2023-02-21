b0c8a6e281b111587cdb49c2e4affc5e25cdf76c
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-21 11:55:41 +0400

    Add events logging with interval

--- src/posts/event-propagation/index.js
+++ src/posts/event-propagation/index.js
@@ -1,15 +1,25 @@
 const elementsIds = ["html", "body", "form", "input", "textarea", "button"]
 
+let eventsAmount = 0
+
 elementsIds.forEach((elementId) => {
   const element = document.querySelector(`.demo #${elementId}`)
-  element.addEventListener("click", () => {
-    console.log({ element: elementId, stage: "bubbling" })
-  })
   element.addEventListener(
     "click",
     () => {
-      console.log({ element: elementId, stage: "capturing" })
+      eventsAmount += 1
+      setTimeout(() => {
+        console.log({ element: elementId, stage: "capturing" })
+        eventsAmount -= 1
+      }, eventsAmount * 500 + 500)
     },
     { capture: true }
   )
+  element.addEventListener("click", () => {
+    eventsAmount += 1
+    setTimeout(() => {
+      console.log({ element: elementId, stage: "capturing" })
+      eventsAmount -= 1
+    }, eventsAmount * 500 + 500)
+  })
 })
