2b85335739861aa82d60b5a6ac5d00693b7f88df
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-22 07:57:08 +0400

    refactor: Highlight layers and arrow with promises

--- src/posts/event-propagation/index.js
+++ src/posts/event-propagation/index.js
@@ -1,46 +1,26 @@
+const wait = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 500)
+  })
+}
+
+const highlightLayer = async ({ layerId }) => {
+  const layer = document.querySelector(`.demo #${layerId}`)
+  layer.classList.add("active")
+  await wait()
+  layer.classList.remove("active")
+}
+
 const elementsIds = ["html", "body", "form", "input", "textarea", "button"]
 
 let eventOccurings = []
 
-const showEventDirection = ({ currentTargetId, phase }) => {
-  let arrow = null
-
-  if (phase === "capturing") {
-    if (currentTargetId === "body") {
-      arrow = document.querySelector("#arrow-from-html-to-body")
-    }
-    if (currentTargetId === "form") {
-      arrow = document.querySelector("#arrow-from-body-to-form")
-    }
-    if (currentTargetId === "input") {
-      arrow = document.querySelector("#arrow-from-form-to-input")
-    }
-    if (currentTargetId === "textarea") {
-      arrow = document.querySelector("#arrow-from-form-to-textarea")
-    }
-    if (currentTargetId === "button") {
-      arrow = document.querySelector("#arrow-from-form-to-button")
-    }
-  }
-
-  if (phase === "bubbling") {
-    if (currentTargetId === "html") {
-      arrow = document.querySelector("#arrow-from-body-to-html")
-    }
-    if (currentTargetId === "body") {
-      arrow = document.querySelector("#arrow-from-form-to-body")
-    }
-    if (currentTargetId === "form") {
-      arrow = document.querySelector("#arrow-from-input-to-form")
-    }
-  }
-
-  if (arrow === null) return
-
+const showEventDirection = async ({ from, to }) => {
+  if (from === to) return
+  const arrow = document.querySelector(`#arrow-from-${from}-to-${to}`)
   arrow.style.visibility = "visible"
-  setTimeout(() => {
-    arrow.style.visibility = "hidden"
-  }, 1000)
+  await wait()
+  arrow.style.visibility = "hidden"
 }
 
 elementsIds.forEach((elementId) => {
@@ -48,31 +28,36 @@ elementsIds.forEach((elementId) => {
   element.addEventListener(
     "click",
     (event) => {
-      eventOccurings.push(event)
-      setTimeout(() => {
-        element.classList.add("active")
-      }, eventOccurings.length * 1500 + 500)
-      setTimeout(() => {
-        showEventDirection({ currentTargetId: elementId, phase: "capturing" })
-      }, eventOccurings.length * 1500)
-      setTimeout(() => {
-        element.classList.remove("active")
-        eventOccurings.shift()
-      }, eventOccurings.length * 1500 + 1500)
+      eventOccurings.push({
+        currentTargetId: event.currentTarget.id,
+        targetId: event.target.id,
+      })
     },
     { capture: true }
   )
   element.addEventListener("click", (event) => {
-    eventOccurings.push(event)
-    setTimeout(() => {
-      element.classList.add("active")
-    }, eventOccurings.length * 1500 + 500)
-    setTimeout(() => {
-      showEventDirection({ currentTargetId: elementId, phase: "bubbling" })
-    }, eventOccurings.length * 1500)
-    setTimeout(() => {
-      element.classList.remove("active")
-      eventOccurings.shift()
-    }, eventOccurings.length * 1500 + 1500)
+    eventOccurings.push({
+      currentTargetId: event.currentTarget.id,
+      targetId: event.target.id,
+    })
   })
+})
+
+document.querySelector(".demo #html").addEventListener("click", () => {
+  setTimeout(async () => {
+    const eventOccuringsMoves = []
+    for (const eventOccuringIndex in eventOccurings) {
+      const currentEventOccuring = eventOccurings[eventOccuringIndex]
+      const nextEventOccuring = eventOccurings[parseInt(eventOccuringIndex) + 1]
+      if (nextEventOccuring === undefined) continue
+      eventOccuringsMoves.push({ from: currentEventOccuring.currentTargetId, to: nextEventOccuring.currentTargetId })
+    }
+    for (const eventOccuringMove of eventOccuringsMoves) {
+      await highlightLayer({ layerId: eventOccuringMove.from })
+      await showEventDirection({ from: eventOccuringMove.from, to: eventOccuringMove.to })
+      await wait()
+    }
+    await highlightLayer({ layerId: eventOccuringsMoves[eventOccuringsMoves.length - 1].to })
+    eventOccurings = []
+  }, 0)
 })
