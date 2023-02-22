dc6b8dcf23d44689b3e8a02853f9f5605c95bbfc
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-02-22 10:27:43 +0400

    feat: Add exact timings for visualization

--- src/posts/event-propagation/index.js
+++ src/posts/event-propagation/index.js
@@ -1,27 +1,37 @@
-const wait = () => {
+let startDate = Date.now()
+
+const wait = (duration) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, 500)
+    setTimeout(resolve, duration)
   })
 }
 
-const highlightLayer = async ({ layerId }) => {
+const highlightLayer = ({ layerId }) => {
+  console.log("Layer highlighting ON:", Date.now() - startDate)
   const layer = document.querySelector(`.demo #${layerId}`)
   layer.classList.add("active")
-  await wait()
-  layer.classList.remove("active")
+  setTimeout(() => {
+    console.log("Layer highlighting OFF:", Date.now() - startDate)
+    layer.classList.remove("active")
+  }, 2000)
+}
+
+const showEventDirection = ({ from, to }) => {
+  if (from === to) return
+  const arrow = document.querySelector(`#arrow-from-${from}-to-${to}`)
+  setTimeout(() => {
+    console.log("Arrow highlighting ON:", Date.now() - startDate)
+    arrow.style.visibility = "visible"
+  }, 1500)
+  setTimeout(() => {
+    console.log("Arrow highlighting OFF:", Date.now() - startDate)
+    arrow.style.visibility = "hidden"
+  }, 3500)
 }
 
 const elementsIds = ["html", "body", "form", "input", "textarea", "button"]
 
 let eventOccurings = []
-
-const showEventDirection = async ({ from, to }) => {
-  if (from === to) return
-  const arrow = document.querySelector(`#arrow-from-${from}-to-${to}`)
-  arrow.style.visibility = "visible"
-  await wait()
-  arrow.style.visibility = "hidden"
-}
 
 elementsIds.forEach((elementId) => {
   const element = document.querySelector(`.demo #${elementId}`)
@@ -45,6 +55,7 @@ elementsIds.forEach((elementId) => {
 
 document.querySelector(".demo #html").addEventListener("click", () => {
   setTimeout(async () => {
+    startDate = Date.now()
     const eventOccuringsMoves = []
     for (const eventOccuringIndex in eventOccurings) {
       const currentEventOccuring = eventOccurings[eventOccuringIndex]
@@ -53,11 +64,12 @@ document.querySelector(".demo #html").addEventListener("click", () => {
       eventOccuringsMoves.push({ from: currentEventOccuring.currentTargetId, to: nextEventOccuring.currentTargetId })
     }
     for (const eventOccuringMove of eventOccuringsMoves) {
-      await highlightLayer({ layerId: eventOccuringMove.from })
-      await showEventDirection({ from: eventOccuringMove.from, to: eventOccuringMove.to })
-      await wait()
+      highlightLayer({ layerId: eventOccuringMove.from })
+      showEventDirection({ from: eventOccuringMove.from, to: eventOccuringMove.to })
+      await wait(3000)
     }
-    await highlightLayer({ layerId: eventOccuringsMoves[eventOccuringsMoves.length - 1].to })
+    const lastEventOccuringMove = eventOccuringsMoves[eventOccuringsMoves.length - 1]
+    highlightLayer({ layerId: lastEventOccuringMove.to })
     eventOccurings = []
   }, 0)
 })
